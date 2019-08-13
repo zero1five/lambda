aff35e1bc5c7c4486faaba929a0ac6300ae871bd
zero1five <zerodengyin@gmail.com>
2019-08-13 12:14:04 +0800

    refactor: refacting webpack ts files

--- packages/lambda-webpack/src/getUserConfig/configs/alias.ts
+++ packages/lambda-webpack/src/getUserConfig/configs/alias.ts
@@ -1,10 +1,11 @@
 import assert from 'assert'
 import { isPlainObject } from 'lodash'
+import { IFWebpackOpts } from '../../../index.d'
 
 export default function() {
   return {
     name: 'alias',
-    validate(val) {
+    validate(val: IFWebpackOpts['alias']) {
       assert(
         isPlainObject(val),
         `The alias config must be Plain Object, but got ${val}`
--- packages/lambda-webpack/src/getUserConfig/configs/autoprefixer.ts
+++ packages/lambda-webpack/src/getUserConfig/configs/autoprefixer.ts
@@ -1,10 +1,11 @@
 import assert from 'assert'
 import { isPlainObject } from 'lodash'
+import { IFWebpackOpts } from '../../../index.d'
 
 export default function() {
   return {
     name: 'autoprefixer',
-    validate(val) {
+    validate(val: IFWebpackOpts['autoprefixer']) {
       assert(
         isPlainObject(val),
         `The autoprefixer config must be Plain Object, but got ${val}`
