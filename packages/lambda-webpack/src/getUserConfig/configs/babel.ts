ea6c7545b1789674516485b950aad0e0c382c4c2
zero1five <zerodengyin@gmail.com>
2019-08-14 21:09:40 +0800

    refactor: refacting webpack ts files

--- packages/lambda-webpack/src/getUserConfig/configs/babel.ts
+++ packages/lambda-webpack/src/getUserConfig/configs/babel.ts
@@ -1,10 +1,11 @@
 import assert from 'assert'
 import { isPlainObject } from 'lodash'
+import { IFWebpackOpts } from '../../..'
 
 export default function() {
   return {
     name: 'babel',
-    validate(val) {
+    validate(val: IFWebpackOpts['babel']) {
       assert(
         isPlainObject(val),
         `The babel config must be Plain Object, but got ${val}`
--- packages/lambda-webpack/src/getUserConfig/configs/browserslist.ts
+++ packages/lambda-webpack/src/getUserConfig/configs/browserslist.ts
@@ -1,9 +1,10 @@
 import assert from 'assert'
+import { IFWebpackOpts } from '../../..'
 
 export default function() {
   return {
     name: 'browserslist',
-    validate(val) {
+    validate(val: IFWebpackOpts['browserslist']) {
       assert(
         Array.isArray(val),
         `The browserslist config must be Array, but got ${val}`
