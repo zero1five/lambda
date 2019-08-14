9220e911986d3bcf2ae04c0bc8cc4e6f79da52c8
zero1five <zerodengyin@gmail.com>
2019-08-14 21:10:46 +0800

    refactor: refacting webpack ts files

--- packages/lambda-webpack/src/getUserConfig/configs/chainConfig.ts
+++ packages/lambda-webpack/src/getUserConfig/configs/chainConfig.ts
@@ -1,9 +1,10 @@
 import assert from 'assert'
+import { IFWebpackOpts } from '../../..'
 
 export default function() {
   return {
     name: 'chainConfig',
-    validate(val) {
+    validate(val: IFWebpackOpts['chainConfig']) {
       assert(
         typeof val === 'function',
         `The chainConfig config must be Function, but got ${val}`
--- packages/lambda-webpack/src/getUserConfig/configs/copy.ts
+++ packages/lambda-webpack/src/getUserConfig/configs/copy.ts
@@ -1,9 +1,10 @@
 import assert from 'assert'
+import { IFWebpackOpts } from '../../..'
 
 export default function() {
   return {
     name: 'copy',
-    validate(val) {
+    validate(val: IFWebpackOpts['copy']) {
       assert(
         Array.isArray(val),
         `The copy config must be Array, but got ${val}`
