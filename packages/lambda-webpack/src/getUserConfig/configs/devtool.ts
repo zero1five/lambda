c4a6ba1a0eac05de6ff287b123b58204c1a26e4e
zero1five <zerodengyin@gmail.com>
2019-08-15 23:18:19 +0800

    refactor: refacting webpack ts files

--- packages/lambda-webpack/src/getUserConfig/configs/devtool.ts
+++ packages/lambda-webpack/src/getUserConfig/configs/devtool.ts
@@ -1,9 +1,10 @@
 import assert from 'assert'
+import { IFWebpackOpts } from '../../..'
 
 export default function() {
   return {
     name: 'devtool',
-    validate(val) {
+    validate(val: IFWebpackOpts['devtool']) {
       assert(
         typeof val === 'string',
         `The devtool config must be String, but got ${val}`
--- packages/lambda-webpack/src/getUserConfig/configs/disableCSSModules.ts
+++ packages/lambda-webpack/src/getUserConfig/configs/disableCSSModules.ts
@@ -1,9 +1,10 @@
 import assert from 'assert'
+import { IFWebpackOpts } from '../../..'
 
 export default function() {
   return {
     name: 'disableCSSModules',
-    validate(val) {
+    validate(val: IFWebpackOpts['disableCSSModules']) {
       assert(
         typeof val === 'boolean',
         `The disableCSSModules config must be Boolean, but got ${val}`
